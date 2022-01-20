0a17504e57bfac61c795cc7dc76e7e0355af6dc6
verifiedcoder <67722271+verifiedcoder@users.noreply.github.com>
2022-01-20 18:08:08 +1000

    Tidy Program.cs, minor tweaks.

--- Src/ReferenceData.Sample/Program.cs
+++ Src/ReferenceData.Sample/Program.cs
@@ -8,34 +8,33 @@ using ReferenceData.Sample.Extensions;
 var sp = new ServiceCollection().AddDbContext<ReferenceDataContext>(options => options.UseInMemoryDatabase("ReferenceDataSample"))
                                 .BuildServiceProvider();
 
-// Use strongly typed enums in your DTOs and models when working with reference data, while maintaining descriptive entries in your database.
+// Use strongly typed enums in your DTOs and models when working with reference data, while maintaining descriptive entries in your database or UI.
 using (var context = sp.GetRequiredService<ReferenceDataContext>())
 {
     // Create the database, which will fire off the seeding of reference data from the source enum.
     Console.WriteLine($"Database Created?: '{context.Database.EnsureCreated()}'{Environment.NewLine}");
     Console.WriteLine($"Seeded Reference Data:{Environment.NewLine}");
 
+    // Read the entities from the database and show how they can be converted to enums.
     context.AvailableColors
            .ToList()
            .ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
 
-    // Read the entities from the database and illustrate how they can be converted to enums for use as strongly typed property values.
+    // Show how enums can be converted to their entity representations.
     Console.WriteLine($"{Environment.NewLine}Convert from Reference Data Entity to Enum:{Environment.NewLine}");
 
     context.AvailableColors
            .ToList()
-           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColor, AvailableColorEntity>)
+           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
            .ToList()
-           .ForEach(c => Console.WriteLine($"Value: '{c.GetValue()}', Property: '{c}', Description: '{c.GetDescription()}'."));
-
-    // Illustrate how enums can be converted to their entity representations.
-    Console.WriteLine($"{Environment.NewLine}Convert from Enum to Reference Data Entity:{Environment.NewLine}");
+           .ForEach(c => Console.WriteLine($"Value: '{c.GetValue()}', Name: '{c}', Description: '{c.GetDescription()}'."));
 
     // Full circle.
+    Console.WriteLine($"{Environment.NewLine}Convert from Reference Data Entity to Enum and back again:{Environment.NewLine}");
+
     context.AvailableColors
            .ToList()
-           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColor, AvailableColorEntity>)
-           .ToList()
+           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
            .Select(ReferenceDataConverter.ConvertEnumToReferenceDataEntity<AvailableColor, AvailableColorEntity>)
            .ToList()
            .ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
