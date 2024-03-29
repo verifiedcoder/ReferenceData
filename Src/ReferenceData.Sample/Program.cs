edf86dba306ef9ad6371f86c3cc4d6468c54db25
verifiedcoder <67722271+verifiedcoder@users.noreply.github.com>
2024-03-29 20:44:41 +1000

    Small updates to source and doc (#11)

--- Src/ReferenceData.Sample/Program.cs
+++ Src/ReferenceData.Sample/Program.cs
@@ -10,32 +10,28 @@ var sp = new ServiceCollection().AddDbContext<ReferenceDataContext>(options => o
 using (var context = sp.GetRequiredService<ReferenceDataContext>())
 {
     // Create the database, which will fire off the seeding of reference data from the source enum.
-    Console.WriteLine($"Database Created?: '{context.Database.EnsureCreated()}'{Environment.NewLine}");
+    Console.WriteLine($"Database Created? '{context.Database.EnsureCreated()}'{Environment.NewLine}");
     Console.WriteLine($"Seeded Reference Data:{Environment.NewLine}");
 
-    // Read the entities from the database and show how they can be converted to enums.
-    context.AvailableColors
-           .ToList()
-           .ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
+    var availableColors = context.AvailableColors.AsNoTracking().ToList();
+
+    // List the reference data entities retrieved from the database.
+    availableColors.ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
 
     // Show how enums can be converted to their entity representations.
     Console.WriteLine($"{Environment.NewLine}Convert from Reference Data Entity to Enum:{Environment.NewLine}");
 
-    context.AvailableColors
-           .ToList()
-           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
+    availableColors.Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
            .ToList()
            .ForEach(c => Console.WriteLine($"Value: '{c.GetValue()}', Name: '{c}', Description: '{c.GetDescription()}'."));
 
     // Full circle.
     Console.WriteLine($"{Environment.NewLine}Convert from Reference Data Entity to Enum and back again:{Environment.NewLine}");
 
-    context.AvailableColors
-           .ToList()
-           .Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
-           .Select(ReferenceDataConverter.ConvertEnumToReferenceDataEntity<AvailableColor, AvailableColorEntity>)
-           .ToList()
-           .ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
+    availableColors.Select(ReferenceDataConverter.ConvertReferenceDataEntityToEnum<AvailableColorEntity, AvailableColor>)
+                   .Select(ReferenceDataConverter.ConvertEnumToReferenceDataEntity<AvailableColor, AvailableColorEntity>)
+                   .ToList()
+                   .ForEach(c => Console.WriteLine($"ID: '{c.Id}', Description: '{c.Description}'."));
 }
 
 Console.ReadLine();
